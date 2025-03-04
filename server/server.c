11d20299219b6e09f02870e97f65881cadbdc212
Brandon Tardio <btardio@gmail.com>
2025-03-04 01:46:23 +0000

    cleanup

--- server/server.c
+++ server/server.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/socket.h>
 #include <sys/types.h>
 #include <syslog.h>
 #include <stdarg.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
-//#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
 
@@ -94,13 +92,6 @@ read_from_client (int filedes)
           perror("Error closing the file");
           return -11;
       }
-
-      //fprintf (stderr, "%s", buffer);
-
-      //return 0;
-      // write to same FD
-      //buffer[nbytes-1] = '~';    
-      //fprintf(stderr, ">>>> %s", buffer);
       
       FILE* file = fopen("/var/tmp/aesdsocketdata", "r");
 
@@ -116,7 +107,6 @@ read_from_client (int filedes)
       size_t bytes_read = fread(fbuffer, 1, file_size, file);
       if (bytes_read != (size_t)file_size) {
         perror("Error reading file");
-        //free(fbuffer);
         fclose(file);
         return 1;
       }
@@ -125,7 +115,6 @@ read_from_client (int filedes)
 
       fclose(file);
 
-      // i dont think you're supposed to be writing back to this
       sbytes = write(filedes, fbuffer, file_size);
       
 
@@ -134,53 +123,6 @@ read_from_client (int filedes)
           perror("write");
           exit(EXIT_FAILURE);
       }
-      //int s_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-      //int c_rval = connect( filedes, (struct sockaddr *) &sockaddrs[filedes], sizeof(sockaddrs[filedes]));
-
-
-      //if (c_rval < 0) {
-      //    log_and_print(LOG_ERR, "Unable to connect.\n", NULL);
-      //    return -1;
-     // }
-
-
-
-// ~~~
-
-
-
-      // end write to same FD
-/*
-      file_pointer = fopen("/var/tmp/aesdsocketdata", "a");
-
-      if ( file_pointer == NULL ){
-          log_and_print(LOG_ERR, "Error writing to file.\n", NULL);
-          return -1;
-      }
-
-      if (fputs(buffer, file_pointer) == EOF) {
-          perror("Error writing to file");
-          fclose(file_pointer);
-          return -1;
-      }
-
-      if (fputs(buffer, file_pointer) == EOF) {
-          perror("Error writing to file");
-          fclose(file_pointer);
-          return -1;
-      }
-      if (fclose(file_pointer) == EOF) {
-          perror("Error closing the file");
-          return -11;
-      }
-
-*/
-
-
-
-
-
 
       return 0;
     }
@@ -216,9 +158,6 @@ make_socket (uint16_t port)
 
 
 
-
-//struct sockaddr sock_address = { AF_INET, "10005" };
-
 void *safe_malloc(size_t n)
 {
     void *p = malloc(n);
@@ -244,33 +183,14 @@ int pmain(void) {
     }
 
 
-    //printf("Server program assignment 5\n");
-
     size_t s_size;
 
     struct in_addr my_s_addr;
 
-    //struct sockaddr_in sock_address;
-
     fd_set active_fd_set, read_fd_set;
 
-    //my_s_addr = inet_addr("127.0.0.1");
     inet_pton(AF_INET, "127.0.0.1", &my_s_addr);
 
-    //inet_aton("127.0.0.1", &sock_address.s_addr);
-    
-    //sock_address.sin_port = 9000;
-
-    //strncpy(&sock_address.sin_port, "10000\0", 6);
-    //sock_address.sin_family = AF_INET;
-    //sock_address.sin_addr = my_s_addr;    
-
-    // socket()
-    //
-    //
-    //
-
-    //int s_fd = socket(PF_INET, SOCK_STREAM, 0);
     int s_fd = make_socket(9000);
 
     if (s_fd < 0) {
@@ -278,41 +198,17 @@ int pmain(void) {
         return -1;
     }
 
-
-//    int b_rval = bind(s_fd, (struct sockaddr *) &sock_address, sizeof(struct sockaddr_in));
-
-//    if ( b_rval < 0 ) {
-//        log_and_print(LOG_ERR, "Unable to bind to port.\n", NULL);
-//    }
-
-    // bind (sockfd, sockaddr-sever)
-    //
-    //
-    //
     
     int l_rval = listen(s_fd, 3);
-
 
 
     if ( l_rval < 0 ) {
         log_and_print(LOG_ERR, "Unable to listen on port.\n", NULL);
     }
-    // listen (sockfd)
-    //
-    //
-    // 
 
     struct sockaddr_in addr_connector;
 
-//    struct socklen_t _s_addr_and_len;
-
-//    struct socklen_t* s_addr_and_len = &s_addr_and_len;
-
-
-    // null null, don't want to deal with privilege mode and traps?
-
     s_size = sizeof (addr_connector);
-    // = 0;
 
 
   /* Initialize the set of active sockets. */
@@ -345,23 +241,7 @@ int pmain(void) {
                     log_and_print(LOG_ERR, "Unable to accept.\n", NULL);
                     
                 } else {
-
-//                new = accept (sock,
-//                              (struct sockaddr *) &clientname,
-//                              &size);
-//                if (new < 0)
-//                  {
-//                    perror ("accept");
-//                    exit (EXIT_FAILURE);
-//                  }
-/*
-                fprintf (stderr,
-                         "Server: connect from host %s, port %hd.\n",
-                         inet_ntoa (addr_connector.sin_addr),
-                         ntohs (addr_connector.sin_port));
-                         */
                     sockaddrs[new] = addr_connector;
-                    //FD_SET (new, &active_fd_set);
                 }
                 FD_SET (new, &active_fd_set);
               }
@@ -376,42 +256,6 @@ int pmain(void) {
               }
           }
     }
-
-    /*
-    for(;;) {
-    
-        // remember: sebastian files, ritchie ports - ritchie you need a pocket protector - trap
-        printf("i am blocking\n");
-        int a_fd = accept(s_fd, (struct sockaddr*) &addr_connector, (unsigned int *) &s_size); //(struct sockaddr *) &addr_connector, NULL);
-         
-        if ( a_fd == -1 ) {
-            log_and_print(LOG_ERR, "Unable to accept.\n", NULL);
-        }
-
-
-        //if (FD_ISSET (i, &read_fd_set))
-
-        read_from_client (s_fd + 1 + 1 + 1);
-        // TODO: Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client. 
-//        vsyslog(LOG_ERR, "Accepted connection from %s\n", inet_ntoa(addr_connector.sin_addr));
-                        fprintf (stderr,
-                         "Server: connect from host %s, port %hd.\n",
-                         inet_ntoa (addr_connector.sin_addr),
-                         ntohs (addr_connector.sin_port));
-
-    }
-*/
-
-    // accept
-    //
-    //
-    //
-
-
-
-
-
-
 }
 
 int main(void){
@@ -427,7 +271,6 @@ int main(void){
         pmain();
     }
     else {
-        //waitpid(p, NULL, 0);
 
     }
 
